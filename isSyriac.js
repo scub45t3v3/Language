4f438849f525e30e1b95e2226d71fcd08871c726
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isSyriac for readability

--- isSyriac.js
+++ isSyriac.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isSyriac;
+'use strict';
 
-  REGEX = /^[\u060C\u061B\u061F\u0640\u064B-\u0655\u0670\u0700-\u070D\u070F-\u074A\u074D-\u074F]+$/;
+(() => {
+  const REGEX = /^[\u060C\u061B\u061F\u0640\u064B-\u0655\u0670\u0700-\u070D\u070F-\u074A\u074D-\u074F]+$/;
 
-  isSyriac = function(value) {
+  const isSyriac = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isSyriac
 
+  // export isSyriac as commonjs module
   module.exports = isSyriac;
-
-}).call(this);
+})(); // end IIFE
