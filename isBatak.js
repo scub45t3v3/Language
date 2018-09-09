caa6e7b2318dc5638016cf57f6ccdeb407f96874
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isBatak for readability

--- isBatak.js
+++ isBatak.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isBatak;
+'use strict';
 
-  REGEX = /^[\u1BC0-\u1BF3\u1BFC-\u1BFF]+$/;
+(() => {
+  const REGEX = /^[\u1BC0-\u1BF3\u1BFC-\u1BFF]+$/;
 
-  isBatak = function(value) {
+  const isBatak = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isBatak
 
+  // export isBatak as commonjs module
   module.exports = isBatak;
-
-}).call(this);
+})(); // end IIFE
