55c6cddffe8c61645610228d7fe1f8b71ce3241b
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isBamum for readability

--- isBamum.js
+++ isBamum.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isBamum;
+'use strict';
 
-  REGEX = /^(?:[\uA6A0-\uA6F7]|\uD81A[\uDC00-\uDE38])+$/;
+(() => {
+  const REGEX = /^(?:[\uA6A0-\uA6F7]|\uD81A[\uDC00-\uDE38])+$/;
 
-  isBamum = function(value) {
+  const isBamum = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isBamum
 
+  // export isBamum as commonjs module
   module.exports = isBamum;
-
-}).call(this);
+})(); // end IIFE
