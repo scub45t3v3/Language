079eb5856fcee2ae46e4fbe616612e6f27f66d3e
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isGlagolitic for readability

--- isGlagolitic.js
+++ isGlagolitic.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isGlagolitic;
+'use strict';
 
-  REGEX = /^(?:[\u0484\u0487\u2C00-\u2C2E\u2C30-\u2C5E\u2E43\uA66F]|\uD838[\uDC00-\uDC06\uDC08-\uDC18\uDC1B-\uDC21\uDC23\uDC24\uDC26-\uDC2A])+$/;
+(() => {
+  const REGEX = /^(?:[\u0484\u0487\u2C00-\u2C2E\u2C30-\u2C5E\u2E43\uA66F]|\uD838[\uDC00-\uDC06\uDC08-\uDC18\uDC1B-\uDC21\uDC23\uDC24\uDC26-\uDC2A])+$/;
 
-  isGlagolitic = function(value) {
+  const isGlagolitic = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isGlagolitic
 
+  // export isGlagolitic as commonjs module
   module.exports = isGlagolitic;
-
-}).call(this);
+})(); // end IIFE
