8e666c32a9d599b459bf3076e5b0274a3f0c0816
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isOldHungarian for readability

--- isOldHungarian.js
+++ isOldHungarian.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isOldHungarian;
+'use strict';
 
-  REGEX = /^(?:\uD803[\uDC80-\uDCB2\uDCC0-\uDCF2\uDCFA-\uDCFF])+$/;
+(() => {
+  const REGEX = /^(?:\uD803[\uDC80-\uDCB2\uDCC0-\uDCF2\uDCFA-\uDCFF])+$/;
 
-  isOldHungarian = function(value) {
+  const isOldHungarian = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isOldHungarian
 
+  // export isOldHungarian as commonjs module
   module.exports = isOldHungarian;
-
-}).call(this);
+})(); // end IIFE
