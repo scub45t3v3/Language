fde66ecbf38488a00193f25c5967d67fe126b478
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isCanadianAboriginal for readability

--- isCanadianAboriginal.js
+++ isCanadianAboriginal.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isCanadianAboriginal;
+'use strict';
 
-  REGEX = /^[\u1400-\u167F\u18B0-\u18F5]+$/;
+(() => {
+  const REGEX = /^[\u1400-\u167F\u18B0-\u18F5]+$/;
 
-  isCanadianAboriginal = function(value) {
+  const isCanadianAboriginal = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isCanadianAboriginal
 
+  // export isCanadianAboriginal as commonjs module
   module.exports = isCanadianAboriginal;
-
-}).call(this);
+})(); // end IIFE
