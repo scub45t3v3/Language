3570ef8242745b8498bd648d5ca5242b1b62f694
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isHatran for readability

--- isHatran.js
+++ isHatran.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isHatran;
+'use strict';
 
-  REGEX = /^(?:\uD802[\uDCE0-\uDCF2\uDCF4\uDCF5\uDCFB-\uDCFF])+$/;
+(() => {
+  const REGEX = /^(?:\uD802[\uDCE0-\uDCF2\uDCF4\uDCF5\uDCFB-\uDCFF])+$/;
 
-  isHatran = function(value) {
+  const isHatran = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isHatran
 
+  // export isHatran as commonjs module
   module.exports = isHatran;
-
-}).call(this);
+})(); // end IIFE
