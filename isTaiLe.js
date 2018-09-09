9d3736e178392dedd04316530d4d4ee446da1fa0
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTaiLe for readability

--- isTaiLe.js
+++ isTaiLe.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTaiLe;
+'use strict';
 
-  REGEX = /^[\u1040-\u1049\u1950-\u196D\u1970-\u1974]+$/;
+(() => {
+  const REGEX = /^[\u1040-\u1049\u1950-\u196D\u1970-\u1974]+$/;
 
-  isTaiLe = function(value) {
+  const isTaiLe = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTaiLe
 
+  // export isTaiLe as commonjs module
   module.exports = isTaiLe;
-
-}).call(this);
+})(); // end IIFE
