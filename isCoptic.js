07ad9b793dc65ca45f60fe860ec0fd5bacc5eb39
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isCoptic for readability

--- isCoptic.js
+++ isCoptic.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isCoptic;
+'use strict';
 
-  REGEX = /^(?:[\u03E2-\u03EF\u2C80-\u2CF3\u2CF9-\u2CFF]|\uD800[\uDEE0-\uDEFB])+$/;
+(() => {
+  const REGEX = /^(?:[\u03E2-\u03EF\u2C80-\u2CF3\u2CF9-\u2CFF]|\uD800[\uDEE0-\uDEFB])+$/;
 
-  isCoptic = function(value) {
+  const isCoptic = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isCoptic
 
+  // export isCoptic as commonjs module
   module.exports = isCoptic;
-
-}).call(this);
+})(); // end IIFE
