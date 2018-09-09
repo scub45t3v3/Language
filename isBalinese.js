8c0e3412afbf4747713c1223ad7b310a15ec791a
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isBalinese for readability

--- isBalinese.js
+++ isBalinese.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isBalinese;
+'use strict';
 
-  REGEX = /^[\u1B00-\u1B4B\u1B50-\u1B7C]+$/;
+(() => {
+  const REGEX = /^[\u1B00-\u1B4B\u1B50-\u1B7C]+$/;
 
-  isBalinese = function(value) {
+  const isBalinese = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isBalinese
 
+  // export isBalinese as commonjs module
   module.exports = isBalinese;
-
-}).call(this);
+})(); // end IIFE
