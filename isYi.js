d587e74b0aec7979f2875e5f26e9c8f623b48f30
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:55 +0700

    refactor isYi for readability

--- isYi.js
+++ isYi.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isYi;
+'use strict';
 
-  REGEX = /^[\u3001\u3002\u3008-\u3011\u3014-\u301B\u30FB\uA000-\uA48C\uA490-\uA4C6\uFF61-\uFF65]+$/;
+(() => {
+  const REGEX = /^[\u3001\u3002\u3008-\u3011\u3014-\u301B\u30FB\uA000-\uA48C\uA490-\uA4C6\uFF61-\uFF65]+$/;
 
-  isYi = function(value) {
+  const isYi = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isYi
 
+  // export isYi as commonjs module
   module.exports = isYi;
-
-}).call(this);
+})(); // end IIFE
