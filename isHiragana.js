f73fd2657ccab542d0495068f18d186b9fc0f5d2
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isHiragana for readability

--- isHiragana.js
+++ isHiragana.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isHiragana;
+'use strict';
 
-  REGEX = /^(?:[\u3001-\u3003\u3008-\u3011\u3013-\u301F\u3030-\u3035\u3037\u303C\u303D\u3041-\u3096\u3099-\u30A0\u30FB\u30FC\uFE45\uFE46\uFF61-\uFF65\uFF70\uFF9E\uFF9F]|\uD82C\uDC01|\uD83C\uDE00)+$/;
+(() => {
+  const REGEX = /^(?:[\u3001-\u3003\u3008-\u3011\u3013-\u301F\u3030-\u3035\u3037\u303C\u303D\u3041-\u3096\u3099-\u30A0\u30FB\u30FC\uFE45\uFE46\uFF61-\uFF65\uFF70\uFF9E\uFF9F]|\uD82C\uDC01|\uD83C\uDE00)+$/;
 
-  isHiragana = function(value) {
+  const isHiragana = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isHiragana
 
+  // export isHiragana as commonjs module
   module.exports = isHiragana;
-
-}).call(this);
+})(); // end IIFE
