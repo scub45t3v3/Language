db7a1b21073b65c2c481795656f3fb0d86126eea
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isNewTaiLue for readability

--- isNewTaiLue.js
+++ isNewTaiLue.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isNewTaiLue;
+'use strict';
 
-  REGEX = /^[\u1980-\u19AB\u19B0-\u19C9\u19D0-\u19DA\u19DE\u19DF]+$/;
+(() => {
+  const REGEX = /^[\u1980-\u19AB\u19B0-\u19C9\u19D0-\u19DA\u19DE\u19DF]+$/;
 
-  isNewTaiLue = function(value) {
+  const isNewTaiLue = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isNewTaiLue
 
+  // export isNewTaiLue as commonjs module
   module.exports = isNewTaiLue;
-
-}).call(this);
+})(); // end IIFE
