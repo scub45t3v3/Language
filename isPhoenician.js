713add89ca468970d86a8f56641f381f62da3349
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isPhoenician for readability

--- isPhoenician.js
+++ isPhoenician.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isPhoenician;
+'use strict';
 
-  REGEX = /^(?:\uD802[\uDD00-\uDD1B\uDD1F])+$/;
+(() => {
+  const REGEX = /^(?:\uD802[\uDD00-\uDD1B\uDD1F])+$/;
 
-  isPhoenician = function(value) {
+  const isPhoenician = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isPhoenician
 
+  // export isPhoenician as commonjs module
   module.exports = isPhoenician;
-
-}).call(this);
+})(); // end IIFE
