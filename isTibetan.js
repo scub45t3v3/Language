591a56ee8b6e308bfcd6b4969e7a1ff9ddf9ef3d
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTibetan for readability

--- isTibetan.js
+++ isTibetan.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTibetan;
+'use strict';
 
-  REGEX = /^[\u0F00-\u0F47\u0F49-\u0F6C\u0F71-\u0F97\u0F99-\u0FBC\u0FBE-\u0FCC\u0FCE-\u0FD4\u0FD9\u0FDA]+$/;
+(() => {
+  const REGEX = /^[\u0F00-\u0F47\u0F49-\u0F6C\u0F71-\u0F97\u0F99-\u0FBC\u0FBE-\u0FCC\u0FCE-\u0FD4\u0FD9\u0FDA]+$/;
 
-  isTibetan = function(value) {
+  const isTibetan = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTibetan
 
+  // export isTibetan as commonjs module
   module.exports = isTibetan;
-
-}).call(this);
+})(); // end IIFE
