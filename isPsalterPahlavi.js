8f6382290b3f8797d2e622fc20382fb268c1ede3
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isPsalterPahlavi for readability

--- isPsalterPahlavi.js
+++ isPsalterPahlavi.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isPsalterPahlavi;
+'use strict';
 
-  REGEX = /^(?:\u0640|\uD802[\uDF80-\uDF91\uDF99-\uDF9C\uDFA9-\uDFAF])+$/;
+(() => {
+  const REGEX = /^(?:\u0640|\uD802[\uDF80-\uDF91\uDF99-\uDF9C\uDFA9-\uDFAF])+$/;
 
-  isPsalterPahlavi = function(value) {
+  const isPsalterPahlavi = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isPsalterPahlavi
 
+  // export isPsalterPahlavi as commonjs module
   module.exports = isPsalterPahlavi;
-
-}).call(this);
+})(); // end IIFE
