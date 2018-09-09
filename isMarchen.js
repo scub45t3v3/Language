db3fab7738414c59c3c485365f46fa01f4256dfa
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isMarchen for readability

--- isMarchen.js
+++ isMarchen.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isMarchen;
+'use strict';
 
-  REGEX = /^(?:\uD807[\uDC70-\uDC8F\uDC92-\uDCA7\uDCA9-\uDCB6])+$/;
+(() => {
+  const REGEX = /^(?:\uD807[\uDC70-\uDC8F\uDC92-\uDCA7\uDCA9-\uDCB6])+$/;
 
-  isMarchen = function(value) {
+  const isMarchen = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isMarchen
 
+  // export isMarchen as commonjs module
   module.exports = isMarchen;
-
-}).call(this);
+})(); // end IIFE
