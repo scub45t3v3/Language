0b85ae6098b84d3fb81fc28cdeca35d4496600a5
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isChakma for readability

--- isChakma.js
+++ isChakma.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isChakma;
+'use strict';
 
-  REGEX = /^(?:[\u09E6-\u09EF\u1040-\u1049]|\uD804[\uDD00-\uDD34\uDD36-\uDD43])+$/;
+(() => {
+  const REGEX = /^(?:[\u09E6-\u09EF\u1040-\u1049]|\uD804[\uDD00-\uDD34\uDD36-\uDD43])+$/;
 
-  isChakma = function(value) {
+  const isChakma = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isChakma
 
+  // export isChakma as commonjs module
   module.exports = isChakma;
-
-}).call(this);
+})(); // end IIFE
