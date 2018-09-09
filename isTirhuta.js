22eda077902d8dddc260f499adc16db284e75790
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTirhuta for readability

--- isTirhuta.js
+++ isTirhuta.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTirhuta;
+'use strict';
 
-  REGEX = /^(?:[\u0964\u0965\uA830-\uA839]|\uD805[\uDC80-\uDCC7\uDCD0-\uDCD9])+$/;
+(() => {
+  const REGEX = /^(?:[\u0964\u0965\uA830-\uA839]|\uD805[\uDC80-\uDCC7\uDCD0-\uDCD9])+$/;
 
-  isTirhuta = function(value) {
+  const isTirhuta = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTirhuta
 
+  // export isTirhuta as commonjs module
   module.exports = isTirhuta;
-
-}).call(this);
+})(); // end IIFE
