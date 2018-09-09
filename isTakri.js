7377f71138e8723c26992124d4ad71c4872e8a95
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTakri for readability

--- isTakri.js
+++ isTakri.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTakri;
+'use strict';
 
-  REGEX = /^(?:[\u0964\u0965\uA830-\uA839]|\uD805[\uDE80-\uDEB7\uDEC0-\uDEC9])+$/;
+(() => {
+  const REGEX = /^(?:[\u0964\u0965\uA830-\uA839]|\uD805[\uDE80-\uDEB7\uDEC0-\uDEC9])+$/;
 
-  isTakri = function(value) {
+  const isTakri = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTakri
 
+  // export isTakri as commonjs module
   module.exports = isTakri;
-
-}).call(this);
+})(); // end IIFE
