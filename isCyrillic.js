916060a9bab30c241fa01b46494d39b33c9e1b8a
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isCyrillic for readability

--- isCyrillic.js
+++ isCyrillic.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isCyrillic;
+'use strict';
 
-  REGEX = /^[\u0400-\u052F\u1C80-\u1C88\u1D2B\u1D78\u2DE0-\u2DFF\u2E43\uA640-\uA69F\uFE2E\uFE2F]+$/;
+(() => {
+  const REGEX = /^[\u0400-\u052F\u1C80-\u1C88\u1D2B\u1D78\u2DE0-\u2DFF\u2E43\uA640-\uA69F\uFE2E\uFE2F]+$/;
 
-  isCyrillic = function(value) {
+  const isCyrillic = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isCyrillic
 
+  // export isCyrillic as commonjs module
   module.exports = isCyrillic;
-
-}).call(this);
+})(); // end IIFE
