273b3e584fbc88e8116d2c7b71a7e54347161224
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isCypriot for readability

--- isCypriot.js
+++ isCypriot.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isCypriot;
+'use strict';
 
-  REGEX = /^(?:\uD800[\uDD00-\uDD02\uDD07-\uDD33\uDD37-\uDD3F]|\uD802[\uDC00-\uDC05\uDC08\uDC0A-\uDC35\uDC37\uDC38\uDC3C\uDC3F])+$/;
+(() => {
+  const REGEX = /^(?:\uD800[\uDD00-\uDD02\uDD07-\uDD33\uDD37-\uDD3F]|\uD802[\uDC00-\uDC05\uDC08\uDC0A-\uDC35\uDC37\uDC38\uDC3C\uDC3F])+$/;
 
-  isCypriot = function(value) {
+  const isCypriot = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isCypriot
 
+  // export isCypriot as commonjs module
   module.exports = isCypriot;
-
-}).call(this);
+})(); // end IIFE
