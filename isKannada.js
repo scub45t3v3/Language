18798696e2ddca0ac5ec2576b54d5cdcaaaa8ead
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isKannada for readability

--- isKannada.js
+++ isKannada.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isKannada;
+'use strict';
 
-  REGEX = /^[\u0951\u0952\u0964\u0965\u0C80-\u0C83\u0C85-\u0C8C\u0C8E-\u0C90\u0C92-\u0CA8\u0CAA-\u0CB3\u0CB5-\u0CB9\u0CBC-\u0CC4\u0CC6-\u0CC8\u0CCA-\u0CCD\u0CD5\u0CD6\u0CDE\u0CE0-\u0CE3\u0CE6-\u0CEF\u0CF1\u0CF2\u1CDA\u1CF5\uA830-\uA835]+$/;
+(() => {
+  const REGEX = /^[\u0951\u0952\u0964\u0965\u0C80-\u0C83\u0C85-\u0C8C\u0C8E-\u0C90\u0C92-\u0CA8\u0CAA-\u0CB3\u0CB5-\u0CB9\u0CBC-\u0CC4\u0CC6-\u0CC8\u0CCA-\u0CCD\u0CD5\u0CD6\u0CDE\u0CE0-\u0CE3\u0CE6-\u0CEF\u0CF1\u0CF2\u1CDA\u1CF5\uA830-\uA835]+$/;
 
-  isKannada = function(value) {
+  const isKannada = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isKannada
 
+  // export isKannada as commonjs module
   module.exports = isKannada;
-
-}).call(this);
+})(); // end IIFE
