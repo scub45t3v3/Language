41f8ede5310b5f275c60ed3d28d6f024300e5fdc
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isHangul for readability

--- isHangul.js
+++ isHangul.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isHangul;
+'use strict';
 
-  REGEX = /^[\u1100-\u11FF\u3001-\u3003\u3008-\u3011\u3013-\u301F\u302E-\u3030\u3037\u30FB\u3131-\u318E\u3200-\u321E\u3260-\u327E\uA960-\uA97C\uAC00-\uD7A3\uD7B0-\uD7C6\uD7CB-\uD7FB\uFE45\uFE46\uFF61-\uFF65\uFFA0-\uFFBE\uFFC2-\uFFC7\uFFCA-\uFFCF\uFFD2-\uFFD7\uFFDA-\uFFDC]+$/;
+(() => {
+  const REGEX = /^[\u1100-\u11FF\u3001-\u3003\u3008-\u3011\u3013-\u301F\u302E-\u3030\u3037\u30FB\u3131-\u318E\u3200-\u321E\u3260-\u327E\uA960-\uA97C\uAC00-\uD7A3\uD7B0-\uD7C6\uD7CB-\uD7FB\uFE45\uFE46\uFF61-\uFF65\uFFA0-\uFFBE\uFFC2-\uFFC7\uFFCA-\uFFCF\uFFD2-\uFFD7\uFFDA-\uFFDC]+$/;
 
-  isHangul = function(value) {
+  const isHangul = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isHangul
 
+  // export isHangul as commonjs module
   module.exports = isHangul;
-
-}).call(this);
+})(); // end IIFE
