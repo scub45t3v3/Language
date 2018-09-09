d90349b71cbe96279c3560339689e9168dcfd722
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isKharoshthi for readability

--- isKharoshthi.js
+++ isKharoshthi.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isKharoshthi;
+'use strict';
 
-  REGEX = /^(?:\uD802[\uDE00-\uDE03\uDE05\uDE06\uDE0C-\uDE13\uDE15-\uDE17\uDE19-\uDE33\uDE38-\uDE3A\uDE3F-\uDE47\uDE50-\uDE58])+$/;
+(() => {
+  const REGEX = /^(?:\uD802[\uDE00-\uDE03\uDE05\uDE06\uDE0C-\uDE13\uDE15-\uDE17\uDE19-\uDE33\uDE38-\uDE3A\uDE3F-\uDE47\uDE50-\uDE58])+$/;
 
-  isKharoshthi = function(value) {
+  const isKharoshthi = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isKharoshthi
 
+  // export isKharoshthi as commonjs module
   module.exports = isKharoshthi;
-
-}).call(this);
+})(); // end IIFE
