2efa8829ff95bc30ec767451133e8eb878ac6584
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isKaithi for readability

--- isKaithi.js
+++ isKaithi.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isKaithi;
+'use strict';
 
-  REGEX = /^(?:[\u0966-\u096F\uA830-\uA839]|\uD804[\uDC80-\uDCC1])+$/;
+(() => {
+  const REGEX = /^(?:[\u0966-\u096F\uA830-\uA839]|\uD804[\uDC80-\uDCC1])+$/;
 
-  isKaithi = function(value) {
+  const isKaithi = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isKaithi
 
+  // export isKaithi as commonjs module
   module.exports = isKaithi;
-
-}).call(this);
+})(); // end IIFE
