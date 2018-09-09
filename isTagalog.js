5f8cee02284372f7951379ca7cbad759dd705b39
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTagalog for readability

--- isTagalog.js
+++ isTagalog.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTagalog;
+'use strict';
 
-  REGEX = /^[\u1700-\u170C\u170E-\u1714\u1735\u1736]+$/;
+(() => {
+  const REGEX = /^[\u1700-\u170C\u170E-\u1714\u1735\u1736]+$/;
 
-  isTagalog = function(value) {
+  const isTagalog = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTagalog
 
+  // export isTagalog as commonjs module
   module.exports = isTagalog;
-
-}).call(this);
+})(); // end IIFE
