67e6dea72160b55b9cb5a69923597a2e67636796
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isSharada for readability

--- isSharada.js
+++ isSharada.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isSharada;
+'use strict';
 
-  REGEX = /^(?:[\u0951\u1CD7\u1CD9\u1CDC\u1CDD\u1CE0]|\uD804[\uDD80-\uDDCD\uDDD0-\uDDDF])+$/;
+(() => {
+  const REGEX = /^(?:[\u0951\u1CD7\u1CD9\u1CDC\u1CDD\u1CE0]|\uD804[\uDD80-\uDDCD\uDDD0-\uDDDF])+$/;
 
-  isSharada = function(value) {
+  const isSharada = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isSharada
 
+  // export isSharada as commonjs module
   module.exports = isSharada;
-
-}).call(this);
+})(); // end IIFE
