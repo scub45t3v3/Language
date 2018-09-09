7cb6dad9461269c2ca77dd94a7b3718d52cbfc5c
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isSignWriting for readability

--- isSignWriting.js
+++ isSignWriting.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isSignWriting;
+'use strict';
 
-  REGEX = /^(?:\uD836[\uDC00-\uDE8B\uDE9B-\uDE9F\uDEA1-\uDEAF])+$/;
+(() => {
+  const REGEX = /^(?:\uD836[\uDC00-\uDE8B\uDE9B-\uDE9F\uDEA1-\uDEAF])+$/;
 
-  isSignWriting = function(value) {
+  const isSignWriting = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isSignWriting
 
+  // export isSignWriting as commonjs module
   module.exports = isSignWriting;
-
-}).call(this);
+})(); // end IIFE
