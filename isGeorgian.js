5c2c00fbb7b4da977f35be05d877c135b309c1fa
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isGeorgian for readability

--- isGeorgian.js
+++ isGeorgian.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isGeorgian;
+'use strict';
 
-  REGEX = /^[\u0589\u10A0-\u10C5\u10C7\u10CD\u10D0-\u10FF\u2D00-\u2D25\u2D27\u2D2D]+$/;
+(() => {
+  const REGEX = /^[\u0589\u10A0-\u10C5\u10C7\u10CD\u10D0-\u10FF\u2D00-\u2D25\u2D27\u2D2D]+$/;
 
-  isGeorgian = function(value) {
+  const isGeorgian = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isGeorgian
 
+  // export isGeorgian as commonjs module
   module.exports = isGeorgian;
-
-}).call(this);
+})(); // end IIFE
