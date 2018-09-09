30c3ca749a3d337a55443f44c270ed47c9f801f6
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isArmenian for readability

--- isArmenian.js
+++ isArmenian.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isArmenian;
+'use strict';
 
-  REGEX = /^[\u0531-\u0556\u0559-\u055F\u0561-\u0587\u0589\u058A\u058D-\u058F\uFB13-\uFB17]+$/;
+(() => {
+  const REGEX = /^[\u0531-\u0556\u0559-\u055F\u0561-\u0587\u0589\u058A\u058D-\u058F\uFB13-\uFB17]+$/;
 
-  isArmenian = function(value) {
+  const isArmenian = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isArmenian
 
+  // export isArmenian as commonjs module
   module.exports = isArmenian;
-
-}).call(this);
+})(); // end IIFE
