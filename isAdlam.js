6c311d52014b22272aa62c537979d726f8c16d61
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isAdlam for readability

--- isAdlam.js
+++ isAdlam.js
@@ -1,12 +1,13 @@
-(function() {
-  var REGEX, isAdlam;
+'use strict';
 
-  REGEX = /^(?:\u0640|\uD83A[\uDD00-\uDD4A\uDD50-\uDD59\uDD5E\uDD5F])+$/;
 
-  isAdlam = function(value) {
+(() => {
+  const REGEX = /^(?:\u0640|\uD83A[\uDD00-\uDD4A\uDD50-\uDD59\uDD5E\uDD5F])+$/;
+
+  const isAdlam = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isAdlam
 
+  // export isAdlam as commonjs module
   module.exports = isAdlam;
-
-}).call(this);
+})(); // end IIFE
