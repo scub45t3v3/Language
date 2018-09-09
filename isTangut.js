e1a9ef64afccaa611bbe4df770627184d224f3af
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTangut for readability

--- isTangut.js
+++ isTangut.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTangut;
+'use strict';
 
-  REGEX = /^(?:\uD81B\uDFE0|[\uD81C-\uD820][\uDC00-\uDFFF]|\uD821[\uDC00-\uDFEC]|\uD822[\uDC00-\uDEF2])+$/;
+(() => {
+  const REGEX = /^(?:\uD81B\uDFE0|[\uD81C-\uD820][\uDC00-\uDFFF]|\uD821[\uDC00-\uDFEC]|\uD822[\uDC00-\uDEF2])+$/;
 
-  isTangut = function(value) {
+  const isTangut = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTangut
 
+  // export isTangut as commonjs module
   module.exports = isTangut;
-
-}).call(this);
+})(); // end IIFE
