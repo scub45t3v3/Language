b6da495b5200da0a8e3045d231fddacf701c78ca
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isHebrew for readability

--- isHebrew.js
+++ isHebrew.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isHebrew;
+'use strict';
 
-  REGEX = /^[\u0591-\u05C7\u05D0-\u05EA\u05F0-\u05F4\uFB1D-\uFB36\uFB38-\uFB3C\uFB3E\uFB40\uFB41\uFB43\uFB44\uFB46-\uFB4F]+$/;
+(() => {
+  const REGEX = /^[\u0591-\u05C7\u05D0-\u05EA\u05F0-\u05F4\uFB1D-\uFB36\uFB38-\uFB3C\uFB3E\uFB40\uFB41\uFB43\uFB44\uFB46-\uFB4F]+$/;
 
-  isHebrew = function(value) {
+  const isHebrew = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isHebrew
 
+  // export isHebrew as commonjs module
   module.exports = isHebrew;
-
-}).call(this);
+})(); // end IIFE
