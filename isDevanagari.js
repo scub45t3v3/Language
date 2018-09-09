e66f7013bd4e68f6185d59356edad5cda42538c6
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isDevanagari for readability

--- isDevanagari.js
+++ isDevanagari.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isDevanagari;
+'use strict';
 
-  REGEX = /^[\u0900-\u097F\u1CD0-\u1CF6\u1CF8\u1CF9\u20F0\uA830-\uA839\uA8E0-\uA8FD]+$/;
+(() => {
+  const REGEX = /^[\u0900-\u097F\u1CD0-\u1CF6\u1CF8\u1CF9\u20F0\uA830-\uA839\uA8E0-\uA8FD]+$/;
 
-  isDevanagari = function(value) {
+  const isDevanagari = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isDevanagari
 
+  // export isDevanagari as commonjs module
   module.exports = isDevanagari;
-
-}).call(this);
+})(); // end IIFE
