6678851e87ac3534d697d5072545664f8a7301a2
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isBopomofo for readability

--- isBopomofo.js
+++ isBopomofo.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isBopomofo;
+'use strict';
 
-  REGEX = /^[\u02EA\u02EB\u3001-\u3003\u3008-\u3011\u3013-\u301F\u302A-\u302D\u3030\u3037\u30FB\u3105-\u312D\u31A0-\u31BA\uFE45\uFE46\uFF61-\uFF65]+$/;
+(() => {
+  const REGEX = /^[\u02EA\u02EB\u3001-\u3003\u3008-\u3011\u3013-\u301F\u302A-\u302D\u3030\u3037\u30FB\u3105-\u312D\u31A0-\u31BA\uFE45\uFE46\uFF61-\uFF65]+$/;
 
-  isBopomofo = function(value) {
+  const isBopomofo = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isBopomofo
 
+  // export isBopomofo as commonjs module
   module.exports = isBopomofo;
-
-}).call(this);
+})(); // end IIFE
