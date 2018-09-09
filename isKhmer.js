c4aa795ea6aa7991eeed92da67aa3b900f56dc9c
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:53 +0700

    refactor isKhmer for readability

--- isKhmer.js
+++ isKhmer.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isKhmer;
+'use strict';
 
-  REGEX = /^[\u1780-\u17DD\u17E0-\u17E9\u17F0-\u17F9\u19E0-\u19FF]+$/;
+(() => {
+  const REGEX = /^[\u1780-\u17DD\u17E0-\u17E9\u17F0-\u17F9\u19E0-\u19FF]+$/;
 
-  isKhmer = function(value) {
+  const isKhmer = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isKhmer
 
+  // export isKhmer as commonjs module
   module.exports = isKhmer;
-
-}).call(this);
+})(); // end IIFE
