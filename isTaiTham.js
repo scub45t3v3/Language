ea2e9349997a40a479156e0a12484316ba874f1a
Steve Winberg <stevewin21@gmail.com>
2018-09-09 13:21:54 +0700

    refactor isTaiTham for readability

--- isTaiTham.js
+++ isTaiTham.js
@@ -1,12 +1,12 @@
-(function() {
-  var REGEX, isTaiTham;
+'use strict';
 
-  REGEX = /^[\u1A20-\u1A5E\u1A60-\u1A7C\u1A7F-\u1A89\u1A90-\u1A99\u1AA0-\u1AAD]+$/;
+(() => {
+  const REGEX = /^[\u1A20-\u1A5E\u1A60-\u1A7C\u1A7F-\u1A89\u1A90-\u1A99\u1AA0-\u1AAD]+$/;
 
-  isTaiTham = function(value) {
+  const isTaiTham = (value) => {
     return REGEX.test(value);
-  };
+  }; // end isTaiTham
 
+  // export isTaiTham as commonjs module
   module.exports = isTaiTham;
-
-}).call(this);
+})(); // end IIFE
